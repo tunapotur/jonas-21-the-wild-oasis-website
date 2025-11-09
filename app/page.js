e39b1feb4effef8dc0148b9c19172ca08cbe989f
Ahmet Tuna POTUR <tpotur@gmail.com>
2025-11-09 19:17:31 +0300

    before starter file

--- app/page.js
+++ app/page.js
@@ -1,3 +1,12 @@
+import Link from "next/link";
+import Navigation from "./components/Navigations";
+
 export default function Page() {
-  return <h1>The Wild Oasis. Welcome to paradise.</h1>;
+  return (
+    <div>
+      <h1>The Wild Oasis. Welcome to paradise.</h1>
+
+      <Link href="/cabins">Explore luxury cabins</Link>
+    </div>
+  );
 }
