6833e579d96a3518067a76a6c184c8cd16cafff1
Ahmet Tuna POTUR <tpotur@gmail.com>
2025-11-14 14:49:49 +0300

    end of the chapter 34-460

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,13 +1,15 @@
+import CabinList from "@/app/_components/CabinList";
+import { Suspense } from "react";
+import Spinner from "../_components/Spinner";
+
+// export const revalidate = 3600;
+// export const revalidate = 60;
+
 export const metadata = {
   title: "Cabins",
 };
 
-import CabinCard from "@/app/_components/CabinCard";
-
 export default function Page() {
-  // CHANGE
-  const cabins = [];
-
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -22,13 +24,9 @@ export default function Page() {
         Welcome to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
