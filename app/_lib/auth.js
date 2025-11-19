3c098d0a6386a9299d68aa98f49ae5b7ca62a9eb
Ahmet Tuna POTUR <tpotur@gmail.com>
2025-11-19 12:45:45 +0300

    lesson

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -22,3 +22,5 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 // https://www.youtube.com/watch?v=XQbiMiNah0o&t=434s
 // https://authjs.dev/getting-started/providers/google
 // http://localhost:3000/api/auth/callback/google
+// https://medium.com/@youngjun625/next-js14-nextauth-v5-3-google-sign-in-3683d8fae69c
+// https://judy-webdecoded.medium.com/how-i-integrated-authentication-next-auth-v5-in-my-nextjs-14-application-0d57f84cf4ac
