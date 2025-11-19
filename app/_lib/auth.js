bec01ec430969fbabe49a6fdeb73904fda9cc230
Ahmet Tuna POTUR <tpotur@gmail.com>
2025-11-19 11:24:52 +0300

    lesson

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,14 +1,24 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
+// export const { handlers, auth, signIn, signOut } = NextAuth({
+//   providers: [Google],
+// });
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [
+    Google({
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+});
+
+// https://www.youtube.com/watch?v=XQbiMiNah0o&t=434s
 // https://authjs.dev/getting-started/providers/google
 // http://localhost:3000/api/auth/callback/google
-
-export const {
-  handlers: { GET, POST },
-  auth,
-  signIn,
-  signOut,
-} = NextAuth({
-  providers: [Google],
-});
--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,1 +1,2 @@
-export { GET, POST } from "@/app/_lib/auth";
+import { handlers } from "@/app/_lib/auth";
+export const { GET, POST } = handlers;
