4283286114e79017aba9776744f18353bb909ebc
Ahmet Tuna POTUR <tpotur@gmail.com>
2025-11-23 14:58:25 +0300

    end of final-5-after-auth

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,8 +1,34 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
+  callbacks: {
+    authorized({ auth, request }) {
+      return !!auth?.user;
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest)
+          await createGuest({ email: user.email, fullName: user.name });
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
 });
 
 // https://authjs.dev/getting-started/authentication/oauth
