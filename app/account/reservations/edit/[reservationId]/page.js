12dde86b9489024b64748fc892e947315294b075
Ahmet Tuna POTUR <tpotur@gmail.com>
2025-11-26 17:26:14 +0300

    first pat of challenges

---
+++ app/account/reservations/edit/[reservationId]/page.js
@@ -0,0 +1,79 @@
+import { auth } from "@/app/_lib/auth";
+import { getBookings } from "@/app/_lib/data-service";
+import Link from "next/link";
+
+export function generateMetadata({ params }) {
+  return { title: `Edit Reservation ${params.reservationId}` };
+}
+
+export default async function Page({ params }) {
+  const session = await auth();
+  const bookings = await getBookings(session.user.guestId);
+  const reservation = bookings.find(
+    (booking) => booking.guestId === session.user.guestId
+  );
+
+  console.log("Reservation:", reservation);
+
+  // CHANGE
+  const reservationId = params.reservationId;
+  const maxCapacity = 23;
+
+  if (reservation.guestId !== session.user.guestId)
+    return (
+      <main className="text-center space-y-6 mt-4">
+        <h1 className="text-3xl font-semibold">This is wrong reservation :(</h1>
+        <Link
+          href="/account/reservations"
+          className="inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg"
+        >
+          Back to yours reservation
+        </Link>
+      </main>
+    );
+
+  return (
+    <div>
+      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+        Edit Reservation #{reservationId}
+      </h2>
+
+      <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+        <div className="space-y-2">
+          <label htmlFor="numGuests">How many guests?</label>
+          <select
+            name="numGuests"
+            id="numGuests"
+            className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
+            required
+          >
+            <option value="" key="">
+              Select number of guests...
+            </option>
+            {Array.from({ length: maxCapacity }, (_, i) => i + 1).map((x) => (
+              <option value={x} key={x}>
+                {x} {x === 1 ? "guest" : "guests"}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="space-y-2">
+          <label htmlFor="observations">
+            Anything we should know about your stay?
+          </label>
+          <textarea
+            name="observations"
+            className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
+          />
+        </div>
+
+        <div className="flex justify-end items-center gap-6">
+          <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
+            Update reservation
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
