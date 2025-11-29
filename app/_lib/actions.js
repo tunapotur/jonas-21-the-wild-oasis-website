587c431b8d172a3bea20b8badac8583890506bc6
Ahmet Tuna POTUR <tpotur@gmail.com>
2025-11-29 19:19:50 +0300

    useOptimisticHook

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -29,6 +29,9 @@ export async function updateGuest(formData) {
 }
 
 export async function deleteReservation(bookingId) {
+  // For testing
+  // await new Promise((res) => setTimeout(res, 2000));
+
   const session = await auth();
   if (!session) throw new Error("You must be logged in");
 
